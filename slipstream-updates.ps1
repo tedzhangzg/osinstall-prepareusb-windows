ee62c6818025a03c968d96916d684d7b227de9ba
Ted Zhang <tedzhangzg@gmail.com>
2025-09-17 14:50:30 +0800

    updated slipstream code

--- slipstream-updates.ps1
+++ slipstream-updates.ps1
@@ -24,58 +24,125 @@ Install - Slipstream Windows Updates
 
 
 
-# include
-# path to 7z
+# define
+$dir_workingdirroot = "C:\slipstream"
+$dir_extractediso = "$dir_workingdirroot\iso"
+$dir_updatefiles = "$dir_workingdirroot\upd"
+$dir_wimmount = "$dir_workingdirroot\mnt"
+$dir_extractedwim = "$dir_extractediso\sources\install.wim"
+
+# create folders
+mkdir $dir_workingdirroot
+mkdir $dir_extractediso
+mkdir $dir_updatefiles
+mkdir $dir_wimmount
+
+# download
+# into "$HOME\Downloads"
+# 
+# iso
+https://www.microsoft.com/en-us/software-download/windows11
+# english (us) x64
+# 
+# update msu
+https://www.catalog.update.microsoft.com/
+# search for yyyy-mm windows 11 x64
+
+# before moving
+# 
+# assume
+# 
+# in "$HOME\Downloads" there are only
+# 1 ISO
+# relevant MSU
+# no other ISO and MSU for other projects
+
+# move downloads
+Get-ChildItem -Path "$HOME\Downloads" -Filter "*.iso" | ForEach-Object { Move-Item -Path $_.FullName -Destination $dir_workingdirroot -Force }
+Get-ChildItem -Path "$HOME\Downloads" -Filter "*.msu" | ForEach-Object { Move-Item -Path $_.FullName -Destination $dir_updatefiles -Force }
+
+# add compression tools to PATH
+# 
+# 7-zip
+if (-Not (Test-Path -Path "$env:ProgramFiles\7-Zip")) {
+    winget install --id "7zip.7zip"
+}
 $env:Path += ";$env:ProgramFiles\7-Zip"
+# or add permanently
+# Add-Content -Path $PROFILE -Value '$env:Path += ";$env:ProgramFiles\7-Zip"'
+# 
+# WinRAR
+# if (-Not (Test-Path -Path "$env:ProgramFiles\WinRAR")) {
+    # winget install --id "RARLab.WinRAR"
+# }
+# $env:Path += ";$env:ProgramFiles\WinRAR"
+# or add permanently
+# Add-Content -Path $PROFILE -Value '$env:Path += ";$env:ProgramFiles\WinRAR"'
 
-# constants
-$path_to_extracted_iso = "$env:SystemDrive\ISO"
-$path_to_downloaded_updates = "$env:SystemDrive\WinUpdates"
-$path_to_mount_folder = "$env:SystemDrive\mnt"
+# extract ISO
+Get-ChildItem -Path "$dir_workingdirroot" -Filter "*.iso" | ForEach-Object { 7z x $_.FullName -o"$dir_extractediso" }
+# 
+# syntax
+# 7z x $path_file -o$dir_dest -p$password
+# unrar x $path_file $dir_dest -p$password
 
-# working folders
-New-Item -ItemType "directory" -Path "$path_to_downloaded_updates" -Force | Out-Null
-New-Item -ItemType "directory" -Path "$path_to_mount_folder" -Force | Out-Null
+# get index
+# 
+Get-WindowsImage -ImagePath $dir_extractedwim
 
-# extract
-# 7z x $path_to_iso -o"$path_to_extracted_iso"
-# eg
-7z x "C:\Users\User\Downloads\Win11_24H2_English_x64.iso" -o"$path_to_extracted_iso"
+# ask installwim_index
+# 
+if ($null -eq $installwim_index) {
+    [int]$installwim_index = Read-Host -Prompt "Enter index "
+}
+# 
+$installwim_index = 6
 
-# move update files
-Invoke-Item -Path "$HOME\Downloads"
-Invoke-Item -Path "$path_to_downloaded_updates"
-# manually move or copy
-
-# determine index
-##### Get-WindowsImage -ImagePath "$path_to_extracted_iso\sources\install.wim"
-dism /get-imageinfo /imagefile:$path_to_extracted_iso\sources\install.wim
-# note SKU number
-# 1 - Home
-# 6 - Pro
-# 10 - Pro for Workstations
-
-# mount
-##### Mount-WindowsImage -ImagePath "$path_to_extracted_iso\sources\install.wim" -Index "6" -Path "$path_to_mount_folder"
-dism /mount-wim /wimfile:$path_to_extracted_iso\sources\install.wim /index:6 /mountdir:$path_to_mount_folder
-
-# slipstream
-##### Add-WindowsPackage -Path "$path_to_mount_folder" -PackagePath "$path_to_downloaded_updates"
-dism /add-package /image:$path_to_mount_folder /PackagePath:$path_to_downloaded_updates
-
-# verify (optional)
-##### Get-WindowsPackage -Path "$path_to_mount_folder"
-dism /image:$path_to_mount_folder /get-packages
-
-# save and unmount
-##### Dismount-WindowsImage -Path "$path_to_mount_folder" -Save
-dism /unmount-wim /mountdir:$path_to_mount_folder /commit
-
-
-# other steps
-dism /cleanup-image /image:"./mnt" /startcomponentcleanup /resetbase
+# mount image
+Get-WindowsImage -ImagePath $dir_extractedwim | Mount-WindowsImage -Index $installwim_index -Path $dir_wimmount
+# Mount-WindowsImage -ImagePath $dir_extractedwim -Index $installwim_index -Path $dir_wimmount
 
 ##################################################
+# start of mounted install.wim
+##################################################
+
+# enable netfx3
+Enable-WindowsOptionalFeature -Path $dir_wimmount -FeatureName "NetFx3" -All -LimitAccess -Source "$dir_extractediso\sources\sxs"
+
+# ask num_kb
+# 
+if ($null -eq $num_kb) {
+    [int]$num_kb = Read-Host -Prompt "Enter KB number "
+}
+# 
+$num_kb = 5065426
+
+# get packages
+Get-WindowsPackage -Path $dir_wimmount
+
+# add packages
+# 
+# install all directly
+Get-WindowsPackage -Path $dir_wimmount | Add-WindowsPackage -PackagePath $dir_updatefiles
+# 
+# no need use powershell loop one by one
+# Get-ChildItem -Path $dir_updatefiles | ForEach-Object { Add-WindowsPackage -Path $dir_wimmount -PackagePath $_.FullName }
+# 
+# no need install one by one
+# Get-ChildItem -Path $dir_updatefiles -Filter "*kb$num_kb*" | ForEach-Object { Add-WindowsPackage -Path $dir_wimmount -PackagePath $_.FullName }
+
+##################################################
+# end of mounted install.wim
+##################################################
+
+# cleanup
+Optimize-WindowsImage -Path $dir_wimmount -StartComponentCleanup
+
+# dismount
+Dismount-WindowsImage -Path $dir_wimmount -Save
+
+# dismount and discard error mounts
+Dismount-WindowsImage -Path $dir_wimmount -Discard
 
 
 
